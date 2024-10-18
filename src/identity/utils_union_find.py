939115cf90b0aa87cd3cf8a19be51b1739d255fd
Mohamed Ashraf Salama <mohamadashrafaast@gmail.com>
2024-10-18 10:20:00 -0400

    exp4: union-find data structure for identity merging

---
+++ src/identity/utils_union_find.py
@@ -0,0 +1,26 @@
+class UnionFind:
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+        self.count_unions = 0
+
+    def find(self, x: int) -> int:
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, a: int, b: int) -> bool:
+        ra, rb = self.find(a), self.find(b)
+        if ra == rb:
+            return False
+        if self.rank[ra] < self.rank[rb]:
+            self.parent[ra] = rb
+        elif self.rank[ra] > self.rank[rb]:
+            self.parent[rb] = ra
+        else:
+            self.parent[rb] = ra
+            self.rank[ra] += 1
+        self.count_unions += 1
+        return True
+
+
